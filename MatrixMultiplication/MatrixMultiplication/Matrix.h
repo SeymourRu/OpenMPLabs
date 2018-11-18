8264b4c6e0d1057210298040523f52c74a8f88be
Seymour <Seymour>
2018-11-18 17:48:13 +0300

    Change int to double, apply refactorings, fix OpenMP prepocessor

--- MatrixMultiplication/MatrixMultiplication/Matrix.h
+++ MatrixMultiplication/MatrixMultiplication/Matrix.h
@@ -6,9 +6,9 @@
 #include <omp.h>
 #include "windows.h"
 
-static std::vector<std::vector<int>> LoadMatrix()
+static std::vector<std::vector<double>> LoadMatrix()
 {
-	std::vector<std::vector<int>> data;
+	std::vector<std::vector<double>> data;
 
 	OPENFILENAME ofn;
 	TCHAR szFile[260] = { 0 };
@@ -31,7 +31,7 @@ static std::vector<std::vector<int>> LoadMatrix()
 
 		if (!file.is_open())
 		{
-			return std::vector<std::vector<int>>();
+			return std::vector<std::vector<double>>();
 		}
 
 		std::string line;
@@ -42,7 +42,7 @@ static std::vector<std::vector<int>> LoadMatrix()
 
 		do
 		{
-			std::vector<int> lineData;
+			std::vector<double> lineData;
 			int value;
 			std::string textedValue;
 			for (int i = 0; i < line.length(); i++)
@@ -73,7 +73,7 @@ static std::vector<std::vector<int>> LoadMatrix()
 				if (lineData.size() != len)
 				{
 					std::cout << "Incorrect matrix, should be balanced" << std::endl;
-					return std::vector<std::vector<int>>();
+					return std::vector<std::vector<double>>();
 				}
 			}
 
@@ -84,23 +84,24 @@ static std::vector<std::vector<int>> LoadMatrix()
 		if (data.size() != len)
 		{
 			std::cout << "Incorrect matrix, should be squared" << std::endl;
-			return std::vector<std::vector<int>>();
+			return std::vector<std::vector<double>>();
 		}
 	}
 
 	return data;
 }
 
-static int CalcDeterminantNormal(std::vector<std::vector<int>> Matrix)
+static double CalcDeterminantNormal(std::vector<std::vector<double>> Matrix)
 {
 	//this function is written in c++ to calculate the determinant of matrix
 	// it's a recursive function that can handle matrix of any dimension
-	int det = 0; // the determinant value will be stored here
-	if (Matrix.size() == 1)
+	double det = 0; // the determinant value will be stored here
+	int n = Matrix.size();
+	if (n == 1)
 	{
 		return Matrix[0][0]; // no calculation needed
 	}
-	else if (Matrix.size() == 2)
+	else if (n == 2)
 	{
 		//in this case we calculate the determinant of a 2-dimensional matrix in a 
 		//default procedure
@@ -111,17 +112,17 @@ static int CalcDeterminantNormal(std::vector<std::vector<int>> Matrix)
 	{
 		//in this case we calculate the determinant of a squared matrix that have 
 		// for example 3x3 order greater than 2
-		for (int p = 0; p < Matrix[0].size(); p++)
+		for (int p = 0; p < n; p++)
 		{
 			//this loop iterate on each elements of the first row in the matrix.
 			//at each element we cancel the row and column it exist in
 			//and form a matrix from the rest of the elements in the matrix
-			std::vector<std::vector<int>> TempMatrix; // to hold the shaped matrix;
-			for (int i = 1; i < Matrix.size(); i++)
+			std::vector<std::vector<double>> TempMatrix; // to hold the shaped matrix;
+			for (int i = 1; i < n; i++)
 			{
 				// iteration will start from row one cancelling the first row values
-				std::vector<int> TempRow;
-				for (int j = 0; j < Matrix[i].size(); j++)
+				std::vector<double> TempRow;
+				for (int j = 0; j < n; j++)
 				{
 					// iteration will pass all cells of the i row excluding the j 
 					//value that match p column
@@ -131,7 +132,9 @@ static int CalcDeterminantNormal(std::vector<std::vector<int>> Matrix)
 					}
 				}
 				if (TempRow.size() > 0)
+				{
 					TempMatrix.push_back(TempRow);
+				}
 				//after adding each row of the new matrix to the vector tempx
 				//we add it to the vector temp which is the vector where the new 
 				//matrix will be formed
@@ -145,16 +148,17 @@ static int CalcDeterminantNormal(std::vector<std::vector<int>> Matrix)
 	}
 }
 
-static int CalcDeterminantOpenMP(std::vector<std::vector<int>> Matrix)
+static double CalcDeterminantOpenMP(std::vector<std::vector<double>> Matrix)
 {
 	//this function is written in c++ to calculate the determinant of matrix
 	// it's a recursive function that can handle matrix of any dimension
-	int det = 0; // the determinant value will be stored here
-	if (Matrix.size() == 1)
+	double det = 0; // the determinant value will be stored here
+	int n = Matrix.size();
+	if (n == 1)
 	{
 		return Matrix[0][0]; // no calculation needed
 	}
-	else if (Matrix.size() == 2)
+	else if (n == 2)
 	{
 		//in this case we calculate the determinant of a 2-dimensional matrix in a 
 		//default procedure
@@ -166,22 +170,24 @@ static int CalcDeterminantOpenMP(std::vector<std::vector<int>> Matrix)
 		//in this case we calculate the determinant of a squared matrix that have 
 		// for example 3x3 order greater than 2
 
-		auto chunk = Matrix.size();
+		//auto chunk = n;
+		int p = 0, i = 1, j = 0;
 
-#pragma omp parallel shared(chunk)
+#pragma omp parallel shared(Matrix,det) firstprivate(p) private(i,j)
 		{
-#pragma omp for schedule(dynamic,chunk) nowait
-			for (int p = 0; p < Matrix[0].size(); p++)
+			//printf("#%d thread \n", omp_get_thread_num());
+#pragma omp for schedule(guided,1) nowait
+			for (p = 0; p < n; p++)
 			{
 				//this loop iterate on each elements of the first row in the matrix.
 				//at each element we cancel the row and column it exist in
 				//and form a matrix from the rest of the elements in the matrix
-				std::vector<std::vector<int>> TempMatrix; // to hold the shaped matrix;
-				for (int i = 1; i < Matrix.size(); i++)
+				std::vector<std::vector<double>> TempMatrix; // to hold the shaped matrix;
+				for (i = 1; i < n; i++)
 				{
 					// iteration will start from row one cancelling the first row values
-					std::vector<int> TempRow;
-					for (int j = 0; j < Matrix[i].size(); j++)
+					std::vector<double> TempRow;
+					for (j = 0; j < n; j++)
 					{
 						// iteration will pass all cells of the i row excluding the j 
 						//value that match p column
@@ -191,12 +197,14 @@ static int CalcDeterminantOpenMP(std::vector<std::vector<int>> Matrix)
 						}
 					}
 					if (TempRow.size() > 0)
+					{
 						TempMatrix.push_back(TempRow);
+					}
 					//after adding each row of the new matrix to the vector tempx
 					//we add it to the vector temp which is the vector where the new 
 					//matrix will be formed
 				}
-				det = det + Matrix[0][p] * pow(-1, p) * CalcDeterminantOpenMP(TempMatrix);
+				det += Matrix[0][p] * pow(-1, p) * CalcDeterminantOpenMP(TempMatrix);
 				//then we calculate the value of determinant by using a recursive way
 				//where we re-call the function by passing to it the new formed matrix
 				//we keep doing this until we get our determinant
@@ -205,3 +213,6 @@ static int CalcDeterminantOpenMP(std::vector<std::vector<int>> Matrix)
 		return det;
 	}
 }
+
+
+
--- MatrixMultiplication/MatrixMultiplication/MatrixMultiplication.cpp
+++ MatrixMultiplication/MatrixMultiplication/MatrixMultiplication.cpp
@@ -3,43 +3,47 @@
 #include <ctime>
 #include <chrono>
 
-void TimerOne(std::vector<std::vector<int>> matrix)
+void BenchForNormal(std::vector<std::vector<double>> matrix)
 {
-	auto matrixSize = matrix.size();
 	auto det = 0;
+	auto matrixSize = matrix.size();
 	int start_s = clock();
 	det = CalcDeterminantNormal(matrix);
 	int stop_s = clock();
 
-	std::cout << "Normal calculation speed: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << std::endl;
+	double durationD = (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000.0;
+	std::cout << "Normal calculation speed: " << durationD << std::endl;
 	std::cout << "Det normal = " << det << " [" << matrixSize << "x" << matrixSize << "]" << std::endl;
 
-	start_s = clock();
-	det = CalcDeterminantOpenMP(matrix);
-	stop_s = clock();
-	std::cout << "OpenMP calculation speed: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << std::endl;
-	std::cout << "Det OpenMP = " << det << " [" << matrixSize << "x" << matrixSize << "]" << std::endl;
-}
-
-void TimerTwo(std::vector<std::vector<int>> matrix)
-{
-	auto matrixSize = matrix.size();
-	auto det = 0;
 	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 	det = CalcDeterminantNormal(matrix);
 	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	auto durationL = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
 
-	std::cout << "Normal calculation speed (ms): " << duration << std::endl;
+	std::cout << "Normal calculation speed (ms): " << durationL << std::endl;
 	std::cout << "Det normal = " << det << " [" << matrixSize << "x" << matrixSize << "]" << std::endl;
+}
 
-	t1 = std::chrono::high_resolution_clock::now();
+void BenchForOpenMP(std::vector<std::vector<double>> matrix)
+{
+	auto det = 0;
+	auto matrixSize = matrix.size();
+
+	int start_s = clock();
 	det = CalcDeterminantOpenMP(matrix);
-	t2 = std::chrono::high_resolution_clock::now();
-	duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	int stop_s = clock();
 
-	std::cout << "OpenMP calculation speed (ms): " << duration << std::endl;
+	double durationD = (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000.0;
+	std::cout << "OpenMP calculation speed: " << durationD << std::endl;
+	std::cout << "Det OpenMP = " << det << " [" << matrixSize << "x" << matrixSize << "]" << std::endl;
+
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	det = CalcDeterminantOpenMP(matrix);
+	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+	auto durationL = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	std::cout << "OpenMP calculation speed (ms): " << durationL << std::endl;
 	std::cout << "Det OpenMP = " << det << " [" << matrixSize << "x" << matrixSize << "]" << std::endl;
 }
 
@@ -49,11 +53,18 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	if (matrix.size() != 0)
 	{
-		for (int i = 0; i < 5; i++)
+		//omp_set_nested(0);
+
+		auto iterationNum = 0;
+
+		std::cout << "Enter num of iterations:" << std::endl;
+		std::cin >> iterationNum;
+
+		for (int i = 0; i < iterationNum; i++)
 		{
 			std::cout << "========================" << std::endl;
-			TimerOne(matrix);
-			TimerTwo(matrix);
+			BenchForNormal(matrix);
+			BenchForOpenMP(matrix);
 			std::cout << "========================" << std::endl;
 		}
 	}
